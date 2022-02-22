a206c3874b2a174f39c44a1a894ae308fd97fc4b
TomaskoxD <tomo.ondrusek@gmail.com>
2022-02-22 13:59:31 +0100

    Split up main into functions

--- hinfosvc.c
+++ hinfosvc.c
@@ -44,8 +44,10 @@ void getString(char *array, char *result, char *delimeter, int get)
 }
 float cpu()
 {
-    char previous[1024] = {0};
-    char current[1024] = {0};
+    char previous[1024], current[1024] = {0};
+    char string_prev[16], string_curr[16] = {0};
+    int64_t numb_prev = 0;
+    int64_t numb_curr = 0;
     FILE *cmd = popen("cat /proc/stat | awk '/^cpu /{flag=1}/^intr/{flag=0}flag'", "r");
     fgets(previous, sizeof(previous), cmd);
     pclose(cmd);
@@ -53,10 +55,6 @@ float cpu()
     cmd = popen("cat /proc/stat | awk '/^cpu /{flag=1}/^intr/{flag=0}flag'", "r");
     fgets(current, sizeof(current), cmd);
     pclose(cmd);
-    char string_prev[16] = {0};
-    char string_curr[16] = {0};
-    int64_t numb_prev = 0;
-    int64_t numb_curr = 0;
     // vypocet vyuzitia procesora
     for (int i = 1; i < 10; i++)
     {
@@ -73,11 +71,57 @@ float cpu()
     return 100 * (float)(numb_curr - numb_prev) / (float)numb_curr;
 }
 
+void fillStruct(struct sockaddr_in *address, int port)
+{
+    address->sin_family = AF_INET;
+    address->sin_addr.s_addr = INADDR_ANY;
+    address->sin_port = htons(port);
+}
+void fillOk(char *msg, char *result)
+{
+    strcpy(msg, "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n");
+    strcat(msg, result);
+}
+void sendResponse(char *info, int new_socket)
+{
+    char result[1024], msg[1024] = {0};
+    if (!strcmp("hostname", info))
+    {
+        FILE *cmd = popen("cat /proc/sys/kernel/hostname", "r"); // ziskanie hostname-u
+        fgets(result, sizeof(result), cmd);
+        pclose(cmd);
+        fillOk(msg, result);
+        send(new_socket, msg, strlen(msg), 0);
+    }
+    else if (!strcmp("cpu-name", info))
+    {
+        FILE *cmd = popen("cat /proc/cpuinfo | grep \"model name\" | head -n 1 | awk -F \':\' \'{print $2}\'", "r"); // ziskanie nazvu procesora
+        fgets(result, sizeof(result), cmd);
+        pclose(cmd);
+        fillOk(msg, result + 1);
+        send(new_socket, msg, strlen(msg), 0);
+    }
+    else if (!strcmp("load", info))
+    {
+        strcpy(result, "");
+        sprintf(result, "%.2f", cpu()); // volanie funkcie na vypocet vyuzitia procesora
+        fillOk(msg, result);
+        strcat(msg, "%");
+        send(new_socket, msg, strlen(msg), 0);
+    }
+    else
+    {
+        strcpy(msg, "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain;\r\n\r\n"); // ak bol zadany iny poziadavok
+        send(new_socket, msg, strlen(msg), 0);
+    }
+    close(new_socket);
+    return;
+}
 int main(int argc, char const *argv[])
 {
     if (argc < 2 || argc == 1) // kontrola argumentov
     {
-        perror("Argument error");
+        fprintf(stderr, "Argument error");
         return (EXIT_FAILURE);
     }
     int port = atoi(argv[1]);
@@ -85,33 +129,27 @@ int main(int argc, char const *argv[])
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char info[1024] = {0};
-    char msg[1024] = {0};
+    char buffer[1024], info[1024];
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) // vytvaranie socketu, zapisanie descriptora do server_fd
     {
-        perror("Socket failed");
+        fprintf(stderr, "Socket failed");
         return (EXIT_FAILURE);
     }
-
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) // pripojenie socketu na port z argumentu
     {
-        perror("Setsockopt failed");
+        fprintf(stderr, "Setsockopt failed");
         return (EXIT_FAILURE);
     }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
-
+    fillStruct(&address, port);
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
-        perror("bind failed");
+        fprintf(stderr, "Bind failed");
         return (EXIT_FAILURE);
     }
     if (listen(server_fd, 3) < 0)
     {
-        perror("listen");
+        fprintf(stderr, "Listen failed");
         return (EXIT_FAILURE);
     }
 
@@ -119,47 +157,14 @@ int main(int argc, char const *argv[])
     {
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
         {
-            perror("accept");
+            fprintf(stderr, "Accept failed");
             return (EXIT_FAILURE);
         }
         valread = read(new_socket, buffer, 1024);
         printf("%s", buffer);
         getString(buffer, info, "/", 2); // ziskanie parametrov z URL
         getString(info, info, " ", 1);
-        char result[1024] = {0};
-        if (!strcmp("hostname", info))
-        {
-            FILE *cmd = popen("cat /proc/sys/kernel/hostname", "r"); // ziskanie hostname-u
-            fgets(result, sizeof(result), cmd);
-            pclose(cmd);
-            strcpy(msg, "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n");
-            strcat(msg, result);
-            send(new_socket, msg, strlen(msg), 0);
-        }
-        else if (!strcmp("cpu-name", info))
-        {
-            FILE *cmd = popen("cat /proc/cpuinfo | grep \"model name\" | head -n 1 | awk -F \':\' \'{print $2}\'", "r"); // ziskanie nazvu procesora
-            fgets(result, sizeof(result), cmd);
-            pclose(cmd);
-            strcpy(msg, "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n");
-            strcat(msg, result);
-            send(new_socket, msg, strlen(msg), 0);
-        }
-        else if (!strcmp("load", info))
-        {
-            strcpy(msg, "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n");
-            strcpy(result, "");
-            sprintf(result, "%.2f", cpu()); // volanie funkcie na vypocet vyuzitia procesora
-            strcat(msg, result);
-            strcat(msg, "%");
-            send(new_socket, msg, strlen(msg), 0);
-        }
-        else
-        {
-            strcpy(msg, "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain;\r\n\r\n"); // ak bol zadany iny poziadavok
-            send(new_socket, msg, strlen(msg), 0);
-        }
-        close(new_socket);
+        sendResponse(info, new_socket);
     }
     return 0;
 }
